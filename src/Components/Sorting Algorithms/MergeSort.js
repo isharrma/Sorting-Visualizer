f7db4d25cc7cbe82a3b02a23b1389b115ee27e67
isharrma <54413011+isharrma@users.noreply.github.com>
2021-04-28 02:36:08 +0530

    Merge Sort Complete

--- src/Components/Sorting Algorithms/MergeSort.js
+++ src/Components/Sorting Algorithms/MergeSort.js
@@ -1,32 +1,93 @@
-
-
-
-
-
-
-export const  MergeSort = (array) => {
-
-    function merge( left,right ){
-        const arr = []; 
-        while(left.length && right.length){
-
-            if(left[0] <right[0])
-                arr.push(left.shift());
-            else
-                arr.push(right.shift());
-        }
-        return [...arr,...left,...right];
-    }
-
-    const sort = array => {
-        let half = array.length/2;
-        if(array.length <2)
-            return array;
-        
-            const left =    array.splice(0,half);
-            return merge(sort(left), sort(array));
-    }
-
-    return sort(array);
-    
+export const MergeSort = (array) => {
+    const animations = [];
+    if (array.length <= 1) return array;
+    const auxiliary = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliary, animations);
+    return animations;
 }
+
+
+
+// // This function  creates partition 
+
+const mergeSortHelper = (mainArray , left , right , auxiliary , animations) => {
+     if (left === right) return;
+
+    const mid = Math.floor((left + right) / 2);
+    mergeSortHelper(auxiliary, left, mid, mainArray, animations);
+    mergeSortHelper(auxiliary, mid+ 1, right, mainArray, animations);
+    merge(mainArray, left, mid, right, auxiliary, animations);
+
+}
+
+
+// // This function merges the partioned array  by checking each member of the partition array. 
+
+const merge = (mainArray , left , mid, right ,auxiliary , animations) =>{
+    let  i = left;
+    let j = mid +1;
+    let k = left;
+
+    while (i <= mid && j <= right) {
+    animations.push([i, j]);                     // 1st = comapring and changing the color
+    animations.push([i, j]);                     // 2nd = compating and finally setting the color to original color
+    if (auxiliary[i] <= auxiliary[j]) {
+      animations.push([k, auxiliary[i]]);
+      mainArray[k++] = auxiliary[i++];
+    } else {
+      animations.push([k, auxiliary[j]]);
+      mainArray[k++] = auxiliary[j++];
+    }
+  }
+  while (i <= mid) {
+    animations.push([i, i]);
+    animations.push([i, i]);
+    animations.push([k, auxiliary[i]]);
+    mainArray[k++] = auxiliary[i++];
+  }
+  while (j <= right) {
+    animations.push([j, j]);
+    animations.push([j, j]);
+    animations.push([k, auxiliary[j]]);
+    mainArray[k++] = auxiliary[j++];
+  }
+}
+
+
+
+
+// // -----------------------------------------------------------------------------
+// // This is an easy way to do  it in JS and above is the one that is on GFG.
+// // -----------------------------------------------------------------------------
+
+
+// // export const  MergeSor = (array) => {
+
+// //     function merge( left,right ){
+// //         const arr = []; 
+// //         while(left.length && right.length){
+
+// //             if(left[0] <right[0])
+// //                 arr.push(left.shift());
+// //             else
+// //                 arr.push(right.shift());
+// //         }
+// //         return [...arr,...left,...right];
+// //     }
+
+// //     const sort = array => {
+// //         let half = array.length/2;
+// //         if(array.length <2)
+// //             return array;
+        
+// //             const left =    array.splice(0,half);
+// //             return merge(sort(left), sort(array));
+// //     }
+
+// //     return sort(array);
+    
+// // }
+  
+
+
+
--- src/Components/SortingVisualizer/SortingVis.jsx
+++ src/Components/SortingVisualizer/SortingVis.jsx
@@ -4,21 +4,60 @@ import {MergeSort} from "../Sorting Algorithms/MergeSort"
 
 import "./SortingVis.css";
 
+
+const ArrayBarNo = 140;
+const PrimaryColor = "#8A2BE2";
+const SecondaryColor = "#20B2AA";
+const AnimationSpeed = 10;
+
+
 const SortingVis = () => {
   const [array, setArray] = useState([]);
 
-  const randomIntFromIntervals = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-  const resetArray = () => {
+   // Handles Generate Array functionality
+   const resetArray = () => {
     const arr = [];
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < ArrayBarNo; i++) {
       arr.push(randomIntFromIntervals(5, 700));
     }
     setArray(arr);
   };
 
+  // Handles Merge Sort animations
+ const mergeSort = () => {
+    const animations = MergeSort(array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange === true) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        console.log(barOneStyle.backgroundColor);
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SecondaryColor: PrimaryColor;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * AnimationSpeed);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * AnimationSpeed);
+      }
+    }
+  }
+
+
+
+  const randomIntFromIntervals = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+ 
+
+ //  To check if the algorithms are working properly or not {Remove at end}
   const testSortingAlgorithms = ()=>{
 
     for(let i=0;i<100;i++){
@@ -50,17 +89,19 @@ const SortingVis = () => {
 
   return (
     <div className="array-container">
-      {array.map((value, idx) => (
-        <div
-          className="array-bar"
-          key={idx}
-          style={{ height: `${value}px` }}
-        ></div>
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PrimaryColor,
+              height: `${value}px`,
+            }}></div>
       ))}
 
       <div className="footer">
         <button onClick={() => resetArray()}>Generate New Array</button>
-        <button onClick={()=>(MergeSort(array={array}))} >Merge Sort</button>
+        <button onClick={ () => mergeSort() } >Merge Sort</button>
         <button>Bubble Sort</button>
         <button>Insertion Sort</button>
         <button>Quick Sort</button>
