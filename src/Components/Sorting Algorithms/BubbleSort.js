af56f6cdd48d7efc779c797a64886a84c8681ee2
isharrma <54413011+isharrma@users.noreply.github.com>
2021-04-28 20:48:41 +0530

    Completed
    Project is Completed but have to add button disable feature.

--- src/Components/Sorting Algorithms/BubbleSort.js
+++ src/Components/Sorting Algorithms/BubbleSort.js
@@ -1,11 +1,90 @@
-import React from 'react'
+// export const BubbleSort = (array) =>{
+//     const animations =[];
+//     if(array.length <= 1) return array;
 
-function BubbleSort() {
-    return (
-        <div>
+//     const auxiliary = array.slice();
+//     bubbleSortHelper(auxiliary ,animations);
+//     return animations;
+// }
+
+// const bubbleSortHelper = (arr , animations) =>{
+
+//   let swapped;
+//     do{ 
+//       swapped=false;
+//         for(let j =0; j<arr.length-1;j++){
+//             animations.push([j, j+1 , true]);
+//             animations.push([j, j ,true]);
+
+//             if(arr[j] > arr[j+1]){
+//             animations.push([j, arr[j+1] , false]);
+//             animations.push([j, arr[j] ,false]);                 
+//                 let temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp; 
+//                 swapped = true;
+//             }
             
-        </div>
-    )
+//         }
+//     }while(swapped)
+// }
+
+
+export function BubbleSort(array){
+
+    const animations =[];
+
+    if(array.length<=1)return array;
+
+    const auxiliaryArray=array.slice();
+
+    bubbleSortHelp(auxiliaryArray,animations);
+
+    return animations;
+
 }
 
-export default BubbleSort
+function bubbleSortHelp(
+    	arr,
+	   animations,
+	    ){
+
+  let sorted;
+//let round = 0;
+  do{
+       sorted=false;
+    for(let i=0;i<arr.length-1;i++)
+
+     
+    {animations.push([i,i+1,true]);
+    
+     animations.push([i,i+1,true]);
+     
+     if(arr[i]>arr[i+1]){
+     
+         animations.push([i,arr[i+1],false]);
+      
+         animations.push([i+1,arr[i],false]);
+        
+         let temp=arr[i];
+        
+         arr[i]=arr[i+1];
+        
+         arr[i+1]=temp;
+        
+         sorted=true;
+      }
+
+      
+    }
+
+  }while(sorted);
+
+}
+
+
+
+
+
+
+
--- src/Components/SortingVisualizer/SortingVis.jsx
+++ src/Components/SortingVisualizer/SortingVis.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import {MergeSort} from "../Sorting Algorithms/MergeSort"
+import {BubbleSort} from "../Sorting Algorithms/BubbleSort"
 
 import "./SortingVis.css";
 
@@ -23,7 +24,7 @@ const SortingVis = () => {
     setArray(arr);
   };
 
-  // Handles Merge Sort animations
+  // ----------Handles Merge Sort animations-------------------------------------
  const mergeSort = () => {
     const animations = MergeSort(array);
     for (let i = 0; i < animations.length; i++) {
@@ -44,42 +45,45 @@ const SortingVis = () => {
           const [barOneIdx, newHeight] = animations[i];
           const barOneStyle = arrayBars[barOneIdx].style;
           barOneStyle.height = `${newHeight}px`;
+        }, i * 5);
+      }
+    }
+  }
+
+
+   // ----------Handles Bubble Sort animations-------------------------------------
+
+   const bubbleSort = () => {
+    const animations = BubbleSort(array);
+    console.log(animations);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = animations[i][2];
+      if (isColorChange === true) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 2 === 0 ? SecondaryColor: PrimaryColor;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * AnimationSpeed);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
         }, i * AnimationSpeed);
       }
     }
   }
 
 
-
+ // ----------Returns Random numbers b/w specified min and max-------------------------------------
   const randomIntFromIntervals = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
- 
-
- //  To check if the algorithms are working properly or not {Remove at end}
-  const testSortingAlgorithms = ()=>{
-
-    for(let i=0;i<100;i++){
-        const arr =[];
-        for(let j=0;j< randomIntFromIntervals(1,1000) ;j++){
-            arr.push(randomIntFromIntervals(-1000,1000));
-        }
-        const jsSortedArray = arr.slice().sort((a,b)=> a-b);
-        const mySortedArray = MergeSort(arr.slice());
-        console.log(arraysAreEqual(jsSortedArray,mySortedArray));
-    }
-  }
-
-  const arraysAreEqual = (arrayOne,arrayTwo) =>{
-    if (arrayOne.length !== arrayTwo.length) return false;
-    for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-  }
 
 
 
@@ -102,10 +106,10 @@ const SortingVis = () => {
       <div className="footer">
         <button onClick={() => resetArray()}>Generate New Array</button>
         <button onClick={ () => mergeSort() } >Merge Sort</button>
-        <button>Bubble Sort</button>
-        <button>Insertion Sort</button>
+        <button onClick={ () => bubbleSort() }>Bubble Sort</button>
+        {/* <button>Insertion Sort</button>
         <button>Quick Sort</button>
-        <button onClick={()=>( testSortingAlgorithms() )} >Test Algo</button>
+        <button onClick={()=>( testSortingAlgorithms() )} >Test Algo</button> */}
         {/* onClick={()=>()} */}
       </div>
     </div>
